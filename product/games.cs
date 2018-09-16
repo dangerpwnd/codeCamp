d80d9a4e4c0d9fb1875b0b47830e150cb0940881
Eric Dangerstone <reric2012@yahoo.com>
2018-09-16 11:11:45 -0500

    Added css variables and class to video for media query change requirement

--- product/games.cs
+++ product/games.cs
@@ -2,3 +2,18 @@
   margin: 0;
   padding: 0;
 }
+
+:root {
+  --video-frame-size: 500px;
+}
+
+@media (max-width: 400px){
+  :root {
+    --video-frame-size: 350px;
+  }
+}
+
+.video {
+  width: var(--video-frame-size, 500px;)
+  height: var(--video-frame-size, 500px;)
+}
